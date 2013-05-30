51e3cc1fafb1fef1fa8cb9761b1345d0f8ecf982
deathcape <metalliteracy@gemail.com>
2013-05-30 23:45:23 +0000

    commit friends_followers__get_friends.py

--- friends_followers__get_friends.py
+++ friends_followers__get_friends.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 import time
@@ -11,7 +12,7 @@ CONSUMER_SECRET = 'HTXxYscwjjDCxJl3FJHgOpJO6oIJvRzpWH6yKncTo'
 APP_NAME = 'deathcapeProject'
 
 SCREEN_NAME = sys.argv[1]
-friends_limit = 10000
+friends_limit = 10000 
 
 def oauth_login(app_name=APP_NAME,consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET,token_file='out/twitter_oauth'):
 
@@ -37,38 +38,6 @@ ids = []
 wait_period = 2 
 cursor = -1
 
-while cursor != 0:
- if wait_period > 3600: 
-  print 'Too many retries. Saving partial data to disk and exiting'
-  f = file('%s.friend_ids' % str(cursor), 'wb')
-  cPickle.dump(ids, f)
-  f.close()
-  exit()
-
- try:
-  response = t.friends.ids(screen_name=SCREEN_NAME, cursor=cursor)
-  ids.extend(response['ids'])
-  wait_period = 2
- except twitter.api.TwitterHTTPError, e:
-  if e.e.code == 401:
-   print 'Encountered 401 Error (Not Authorized)'
-   print 'User %s is protecting their tweets' % (SCREEN_NAME, )
-  elif e.e.code in (502, 503):
-   print 'Encountered %i Error. Trying again in %i seconds' % (e.e.code, wait_period)
-   time.sleep(wait_period)
-   wait_period *= 1.5
-   continue
-  elif t.account.rate_limit_status()['remaining_hits'] == 0:
-   status = t.account.rate_limit_status()
-   now = time.time() # UTC
-   when_rate_limit_resets = status['reset_time_in_seconds'] # UTC
-   sleep_time = when_rate_limit_resets - now
-   print 'Rate limit reached. Trying again in %i seconds' % (sleep_time,)
-   time.sleep(sleep_time)
-   continue
- cursor = response['next_cursor']
- print 'Fetched %i ids for %s' % (len(ids), SCREEN_NAME)
- if len(ids) >= friends_limit:
-  break
-
-print ids
+r = t.users.show.name(screen_name='deathcape',include_entities='id')
+for t in r:
+	print t.id
